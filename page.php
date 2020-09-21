a163bcc02b2e9b551b4f1b04cb98fc0fb43ea6bb
mboudakian <michelle.boudakian@gmail.com>
2020-09-20 23:23:18 -0300

    prueba-institucional

--- page.php
+++ page.php
@@ -1,16 +1,30 @@
-<?php get_header();?>
-<!-- contenido de la pagina -->
-<div class="container">
 <?php
+
+/*
+Template name: Institucional
+*/
+
+get_header();?>
+<!-- contenido de la pagina -->
+
+<section class="page">
+    <div class="container">
+            <div class="row py-5">
+               <?php
         if (have_posts()) :
             while (have_posts()) : the_post(); ?>
 <h2><?php the_title(); ?></h2>
 <div class="text-justify">
    <?php the_content()?>
 </div>
-        <?php endwhile; endif; ?>
+        <?php endwhile; endif; ?>     
+            </div>
 
-</div>
+
+</div>    
+</section>
+
+
 
 <!-- fin contenido de la pagina -->
 
