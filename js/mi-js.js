0b70719b6cd92548c62c3944c67173a5b6b7b346
mboudakian <michelle.boudakian@gmail.com>
2020-09-20 21:27:37 -0300

    js en logo del header

---
+++ js/mi-js.js
@@ -0,0 +1,12 @@
+
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("header-conacai").className = "";
+  } else {
+    document.getElementById("header-conacai").className = "logo-oculto";
+  }
+}
+
